65a45fb19fe5054643609db34a9f601b989351ed
Ankit Patwal <ankitptwl4@gmail.com>
2024-07-23 12:44:44 +0530

    Update BackTracking.java

--- BackTracking.java
+++ BackTracking.java
@@ -1,4 +1,79 @@
 
+//regex matching
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        
+        // int stringSize=s.length();
+
+        // int patternSize=p.length();
+
+        Boolean dp[][] = new Boolean[s.length()+1][p.length()+1];
+
+      
+
+        boolean isMatched = matchStringAndPattern(s,p,0,0,dp);
+
+        return isMatched;
+    }
+
+    
+    public boolean matchStringAndPattern(String s, String p, int i, int j,Boolean[][] dp) {
+        
+            // base case
+            // if both strings are traversed completely
+
+            if(i>=s.length() && j>=p.length()){
+                return true;
+            }
+
+            if(j>=p.length()){
+                return false;
+            }
+
+            if (dp[i][j] != null) {
+               return dp[i][j];
+            }
+
+            boolean firstCharMatched=false;
+
+            if(i<s.length()){
+                
+                if(s.charAt(i)==p.charAt(j)){
+                    firstCharMatched=true;
+                }
+                else if(p.charAt(j)=='.'){
+                    firstCharMatched=true;
+                } 
+            }
+            
+
+            if( j+1<p.length() && p.charAt(j+1)=='*'){
+
+                // we have 2 scenarios
+
+                // check if need to ignore * and do not match chars and jump to next char
+                    boolean notIncludeStar=matchStringAndPattern(s,p,i,j+2,dp);
+
+                // or match all preceding chars before * with string chars
+                    boolean includeStar=firstCharMatched && matchStringAndPattern(s,p,i+1,j,dp);
+
+                    dp[i][j] = notIncludeStar || includeStar;
+
+                    //dp[i][j]=matched;
+            }else{
+                    if(firstCharMatched){
+                        dp[i][j]=matchStringAndPattern(s,p,i+1,j+1,dp);
+                    }else{
+                        dp[i][j]=false;
+                    }
+            }
+
+            return dp[i][j];
+
+        }
+    }
+
 
 // all possible subsets of array
 
